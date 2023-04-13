b559cfbefac1dfe91efa48203792da6d862ae336
Bhavya <bhavyajain768@gmail.com>
2023-04-14 02:16:47 +0530

    updated heading

--- src/main/java/com/example/calculator/CalControl/CalculatorControl.java
+++ src/main/java/com/example/calculator/CalControl/CalculatorControl.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Controller
 public class CalculatorControl {
@@ -16,7 +19,7 @@ public class CalculatorControl {
     }
 
     @PostMapping("/calculate")
-    public String addForm(Calculator calculator,Model model){
+    public String addForm(Calculator calculator, Model model){
         model.addAttribute("Calculator",calculator);
         return "result";
     }
